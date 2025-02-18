381edd66d5629e314d2ca990dfaa1d57c6a18526
Yaco <yacoyaco@gmail.com>
2025-02-18 17:42:21 -0300

    agregado array de objetos

---
+++ script.js
@@ -0,0 +1,12 @@
+const objects = [
+    { id: 'objeto1', imagen: 'url(./img/ballena.jpg)' },
+    { id: 'objeto2', imagen: 'url(./img/pez.jpg)' },
+    { id: 'objeto3', imagen: 'url(./img/ralla.jpg)' },
+  ];
+
+const piezas = document.querySelectorAll('.piece');
+
+const assignImag = () => {
+    
+    let duplicateObjetcs = [...objects, ...objects];
+}
